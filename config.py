0ccbe4a2f8bafa10799b3f9805309c24789bd141
root <root@Majeed>
2023-01-31 02:19:06 +0300

    updates

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://doadmin:AVNS_eUobwkwcx5EWK1Fk1In@fyyur-do-user-13431804-0.b.db.ondigitalocean.com:25060/defaultdb'
+DATABASE_URI = 'postgres://mgrhdzaaxfrqvk:9fe43ec6f2cd2e3222ca3afa875059e4008916205b9e71e002b8fc558572d2fd@ec2-3-229-252-6.compute-1.amazonaws.com:5432/dc5jm3ce22aapv'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
